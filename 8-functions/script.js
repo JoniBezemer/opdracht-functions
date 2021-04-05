01d9c9282cf560969b9e37b80962c8092dd10867
JoniBezemer <77683711+JoniBezemer@users.noreply.github.com>
2021-04-05 12:54:46 +0200

    Add files via upload

---
+++ 8-functions/script.js
@@ -0,0 +1,24 @@
+let makePizza = function() {
+    console.log('maak deeg');
+    console.log('beleg het pizza deeg');
+    console.log('stop in de oven');
+};
+
+makePizza();
+makePizza();
+
+
+// ja ik krijg de strings te zin in mijn cmd node en in de terminal in dev tools
+
+let makeSushi = function(){
+    console.log('kook de sushi rijst');
+    console.log('snij de zalm en avocado in reepjes');
+    console.log('giet de rijst af en laat afkoelen');
+    console.log('leg een dunne plak rijst op het nori vel');
+    console.log('leg de zalm en avocado op de rijst');
+    console.log('draai de rol');
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
